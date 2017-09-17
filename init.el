dcb05eaae4c2b7f9fca7e01edea4f8d8de72fb25
Pascal Huber <pascal.huber@resolved.ch>
2017-09-17 18:13:23 +0200

    added fixme-mode

--- init.el
+++ init.el
@@ -1,2 +1,16 @@
 (package-initialize)
 (org-babel-load-file "~/.emacs.d/configuration.org")
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (fixme-mode yaml-mode tern-auto-complete smex simpleclip ruby-electric rainbow-delimiters org-caldav openwith neotree mu4e-maildirs-extension mu4e-alert markdown-mode magit lua-mode js2-mode jedi helm-projectile fsharp-mode fill-column-indicator evil-org evil-mu4e evil-leader evil-ediff emojify elpy elfeed-org elfeed-goodies drag-stuff dictionary dictcc calfw auctex ac-math))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(show-paren-match ((((class color) (background dark)) (:background "firebrick")))))
