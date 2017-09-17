3d8407b4c7304cde2bc0ded461e1b7deb108d5c0
Pascal Huber <pascal.huber@resolved.ch>
2017-09-18 00:49:22 +0200

    mu4e dont reply to myself

--- init.el
+++ init.el
@@ -7,7 +7,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (evil-god-state fixme-mode yaml-mode tern-auto-complete smex simpleclip ruby-electric rainbow-delimiters org-caldav openwith neotree mu4e-maildirs-extension mu4e-alert markdown-mode magit lua-mode js2-mode jedi helm-projectile fsharp-mode fill-column-indicator evil-org evil-mu4e evil-leader evil-ediff emojify elpy elfeed-org elfeed-goodies drag-stuff dictionary dictcc calfw auctex ac-math))))
+    (org-bullets evil-god-state fixme-mode yaml-mode tern-auto-complete smex simpleclip ruby-electric rainbow-delimiters org-caldav openwith neotree mu4e-maildirs-extension mu4e-alert markdown-mode magit lua-mode js2-mode jedi helm-projectile fsharp-mode fill-column-indicator evil-org evil-mu4e evil-leader evil-ediff emojify elpy elfeed-org elfeed-goodies drag-stuff dictionary dictcc calfw auctex ac-math))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
