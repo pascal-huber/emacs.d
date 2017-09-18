36945ead02d5f0724b509e85ad4c7201f9044639
Pascal Huber <pascal.huber@resolved.ch>
2017-09-18 21:49:22 +0200

    linum mode only for programming

--- init.el
+++ init.el
@@ -1,18 +1,10 @@
+(package-initialize)
+(org-babel-load-file "~/.emacs.d/configuration.org")
+(custom-set-faces
+ '(show-paren-match ((((class color) (background dark)) (:background "firebrick")))))
 (custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
  '(ansi-color-names-vector
    ["#242424" "#e5786d" "#95e454" "#cae682" "#8ac6f2" "#333366" "#ccaa8f" "#f6f3e8"])
  '(custom-safe-themes
    (quote
     ("ba7917b02812fee8da4827fdf7867d3f6f282694f679b5d73f9965f45590843a" default))))
-(package-initialize)
-(org-babel-load-file "~/.emacs.d/configuration.org")
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(show-paren-match ((((class color) (background dark)) (:background "firebrick")))))
