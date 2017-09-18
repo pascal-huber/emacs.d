0a98c213208c1cf6c113ac6d7d56ca5ebd355908
Pascal Huber <pascal.huber@resolved.ch>
2017-09-18 20:47:36 +0200

    cleaned and redefined keybindings

--- init.el
+++ init.el
@@ -1,13 +1,15 @@
-(package-initialize)
-(org-babel-load-file "~/.emacs.d/configuration.org")
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages
+ '(ansi-color-names-vector
+   ["#242424" "#e5786d" "#95e454" "#cae682" "#8ac6f2" "#333366" "#ccaa8f" "#f6f3e8"])
+ '(custom-safe-themes
    (quote
-    (org-bullets evil-god-state fixme-mode yaml-mode tern-auto-complete smex simpleclip ruby-electric rainbow-delimiters org-caldav openwith neotree mu4e-maildirs-extension mu4e-alert markdown-mode magit lua-mode js2-mode jedi helm-projectile fsharp-mode fill-column-indicator evil-org evil-mu4e evil-leader evil-ediff emojify elpy elfeed-org elfeed-goodies drag-stuff dictionary dictcc calfw auctex ac-math))))
+    ("ba7917b02812fee8da4827fdf7867d3f6f282694f679b5d73f9965f45590843a" default))))
+(package-initialize)
+(org-babel-load-file "~/.emacs.d/configuration.org")
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
