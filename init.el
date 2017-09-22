00aa22c2d0d0bd99b57818ee7b6f029937a290f9
Pascal Huber <pascal.huber@resolved.ch>
2017-09-22 18:05:46 +0200

    restructure

--- init.el
+++ init.el
@@ -1,10 +1,22 @@
 (package-initialize)
 (org-babel-load-file "~/.emacs.d/configuration.org")
 (custom-set-faces
- '(show-paren-match ((((class color) (background dark)) (:background "firebrick")))))
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
 (custom-set-variables
- '(ansi-color-names-vector
-   ["#242424" "#e5786d" "#95e454" "#cae682" "#8ac6f2" "#333366" "#ccaa8f" "#f6f3e8"])
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-faces-vector
+   [default default default italic underline success warning error])
  '(custom-safe-themes
    (quote
-    ("ba7917b02812fee8da4827fdf7867d3f6f282694f679b5d73f9965f45590843a" default))))
+    ("b9cbfb43711effa2e0a7fbc99d5e7522d8d8c1c151a3194a4b176ec17c9a8215" "9a155066ec746201156bb39f7518c1828a73d67742e11271e4f24b7b178c4710" "43c1a8090ed19ab3c0b1490ce412f78f157d69a29828aa977dae941b994b4147" "67e998c3c23fe24ed0fb92b9de75011b92f35d3e89344157ae0d544d50a63a72" "0c387e27a3dd040b33c6711ff92e13bd952369a788eee97e4e4ea2335ac5528f" "aae95fc700f9f7ff70efbc294fc7367376aa9456356ae36ec234751040ed9168" "b9a06c75084a7744b8a38cb48bc987de10d68f0317697ccbd894b2d0aca06d2b" "3629b62a41f2e5f84006ff14a2247e679745896b5eaa1d5bcfbc904a3441b0cd" "ba7917b02812fee8da4827fdf7867d3f6f282694f679b5d73f9965f45590843a" default)))
+ '(hl-sexp-background-color "#efebe9")
+ '(package-selected-packages
+   (quote
+    (color-theme-sanityinc-tomorrow zenburn-theme bubbleberry-theme distinguished-theme moe-theme monokai-theme ac-emacs-eclim company-emacs-eclim eclim yaml-mode tern-auto-complete smex simpleclip ruby-electric rainbow-delimiters org-caldav org-bullets openwith neotree mu4e-maildirs-extension mu4e-alert meghanada markdown-mode magit lua-mode leuven-theme js2-mode jedi helm-projectile fsharp-mode fixme-mode fill-column-indicator evil-org evil-mu4e evil-leader evil-god-state evil-ediff emojify elpy elfeed-org elfeed-goodies drag-stuff dictionary dictcc calfw auctex ac-math))))
