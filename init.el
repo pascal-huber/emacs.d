3d7b23aceb6c3b9a83520a54fc9ed88dc2439040
Pascal Huber <pascal.huber@resolved.ch>
2017-12-23 12:49:26 +0100

    spaceline and attachment-check

--- init.el
+++ init.el
@@ -19,4 +19,8 @@
  '(hl-sexp-background-color "#efebe9")
  '(package-selected-packages
    (quote
-    (spaceline magit airline-themes powerline-evil dumb-jump pug-mode eruby-mode haml-mode grizzl folding web-mode web-beautify color-theme-sanityinc-tomorrow zenburn-theme bubbleberry-theme distinguished-theme moe-theme monokai-theme ac-emacs-eclim company-emacs-eclim eclim yaml-mode tern-auto-complete smex simpleclip rainbow-delimiters org-caldav org-bullets openwith neotree mu4e-maildirs-extension mu4e-alert meghanada markdown-mode lua-mode leuven-theme js2-mode jedi helm-projectile fsharp-mode fixme-mode fill-column-indicator evil-org evil-mu4e evil-leader evil-god-state evil-ediff emojify elpy elfeed-org elfeed-goodies drag-stuff dictionary dictcc calfw auctex ac-math))))
+    (spaceline-all-the-icons use-package spaceline magit airline-themes powerline-evil dumb-jump pug-mode eruby-mode haml-mode grizzl folding web-mode web-beautify color-theme-sanityinc-tomorrow zenburn-theme bubbleberry-theme distinguished-theme moe-theme monokai-theme ac-emacs-eclim company-emacs-eclim eclim yaml-mode tern-auto-complete smex simpleclip rainbow-delimiters org-caldav org-bullets openwith neotree mu4e-maildirs-extension mu4e-alert meghanada markdown-mode lua-mode leuven-theme js2-mode jedi helm-projectile fsharp-mode fixme-mode fill-column-indicator evil-org evil-mu4e evil-leader evil-god-state evil-ediff emojify elpy elfeed-org elfeed-goodies drag-stuff dictionary dictcc calfw auctex ac-math)))
+ '(spaceline-all-the-icons-clock-always-visible nil)
+ '(spaceline-all-the-icons-flycheck-alternate nil)
+ '(spaceline-all-the-icons-icon-set-vc-icon-git (quote git-name))
+ '(spaceline-all-the-icons-separator-type (quote none)))
