6387edcc1b251395510de13091b039c2346dc201
Pascal Huber <pascal.huber@resolved.ch>
2018-02-22 09:39:24 +0100

    spaceline, elfeed, php-mode

--- init.el
+++ init.el
@@ -1,14 +1,13 @@
 (package-initialize)
 (org-babel-load-file "~/.emacs.d/configuration.org")
 (custom-set-faces
- '(powerline-active1 ((t (:inherit mode-line :background "gray66" :foreground "black"))))
- '(powerline-active2 ((t (:inherit mode-line :background "gray40" :foreground "black"))))
- '(mode-line ((t (:inherit mode-line :background "gray53" :foreground "black"))))
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(mode-line ((t (:inherit mode-line :background "gray53" :foreground "black"))))
+ '(powerline-active1 ((t (:inherit mode-line :background "gray66" :foreground "black"))))
+ '(powerline-active2 ((t (:inherit mode-line :background "gray40" :foreground "black")))))
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -18,8 +17,8 @@
    [default default default italic underline success warning error])
  '(custom-safe-themes
    (quote
-    ("a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" default)))
+    ("a19265ef7ecc16ac4579abb1635fd4e3e1185dcacbc01b7a43cf7ad107c27ced" "a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" default)))
  '(hl-sexp-background-color "#efebe9")
  '(package-selected-packages
    (quote
-    (virtualenvwrapper spaceline-all-the-icons use-package spaceline magit airline-themes powerline-evil dumb-jump pug-mode eruby-mode haml-mode grizzl folding web-mode web-beautify color-theme-sanityinc-tomorrow zenburn-theme bubbleberry-theme distinguished-theme moe-theme monokai-theme ac-emacs-eclim company-emacs-eclim eclim yaml-mode tern-auto-complete smex simpleclip rainbow-delimiters org-caldav org-bullets openwith neotree mu4e-maildirs-extension mu4e-alert meghanada markdown-mode lua-mode leuven-theme js2-mode jedi helm-projectile fsharp-mode fixme-mode fill-column-indicator evil-org evil-mu4e evil-leader evil-god-state evil-ediff emojify elpy elfeed-org elfeed-goodies drag-stuff dictionary dictcc calfw auctex ac-math))))
+    (php-mode virtualenvwrapper spaceline-all-the-icons use-package spaceline magit airline-themes powerline-evil dumb-jump pug-mode eruby-mode haml-mode grizzl folding web-mode web-beautify color-theme-sanityinc-tomorrow zenburn-theme bubbleberry-theme distinguished-theme moe-theme monokai-theme ac-emacs-eclim company-emacs-eclim eclim yaml-mode tern-auto-complete smex simpleclip rainbow-delimiters org-caldav org-bullets openwith neotree mu4e-maildirs-extension mu4e-alert meghanada markdown-mode lua-mode leuven-theme js2-mode jedi helm-projectile fsharp-mode fixme-mode fill-column-indicator evil-org evil-mu4e evil-leader evil-god-state evil-ediff emojify elpy elfeed-org elfeed-goodies drag-stuff dictionary dictcc calfw auctex ac-math))))
