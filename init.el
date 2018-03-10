9ad5096fbc017849720aa27e63b9867ea6447741
Pascal Huber <pascal.huber@resolved.ch>
2018-03-10 17:23:26 +0100

    refactored theme-rotate

--- init.el
+++ init.el
@@ -6,7 +6,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(mode-line ((t (:font "DejaVuSansMono Nerd Font 10"))))
- '(mode-line-inactive ((t (:inherit 'mode-line)))))
+ '(mode-line-inactive ((t (:inherit (quote mode-line))))))
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -16,7 +16,7 @@
    [default default default italic underline success warning error])
  '(custom-safe-themes
    (quote
-    ("a19265ef7ecc16ac4579abb1635fd4e3e1185dcacbc01b7a43cf7ad107c27ced" "a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" default)))
+    ("b9a06c75084a7744b8a38cb48bc987de10d68f0317697ccbd894b2d0aca06d2b" "a19265ef7ecc16ac4579abb1635fd4e3e1185dcacbc01b7a43cf7ad107c27ced" "a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" default)))
  '(hl-sexp-background-color "#efebe9")
  '(package-selected-packages
    (quote
