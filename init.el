50a8c0457384ba967ccd68c2a5a6daa52433b96b
Pascal Huber <pascal.huber@resolved.ch>
2018-08-13 19:29:39 +0200

    restructure and various improvements

--- init.el
+++ init.el
@@ -1,24 +1,17 @@
 (package-initialize)
 (org-babel-load-file "~/.emacs.d/configuration.org")
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(mode-line ((t (:font "DejaVuSansMono Nerd Font 10"))))
- '(mode-line-inactive ((t (:inherit (quote mode-line))))))
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(ansi-color-faces-vector
-   [default default default italic underline success warning error])
- '(custom-safe-themes
-   (quote
-    ("a24c5b3c12d147da6cef80938dca1223b7c7f70f2f382b26308eba014dc4833a" "b9a06c75084a7744b8a38cb48bc987de10d68f0317697ccbd894b2d0aca06d2b" "a19265ef7ecc16ac4579abb1635fd4e3e1185dcacbc01b7a43cf7ad107c27ced" "a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" default)))
- '(hl-sexp-background-color "#efebe9")
  '(package-selected-packages
    (quote
-    (auto-package-update jquery-doc company-quickhelp ## robe enh-ruby-mode posframe flycheck-pos-tip magit-todos smartparens ido-vertical-mode elfeed-protocol json-mode synosaurus ivy counsel-bbdb 0blayout counsel material-theme inf-ruby clj-refactor which-key ac-cider cider org-ref default-text-scale evil-escape free-keys unbound realgud evil-nerd-commenter helm-ag diminish comment-tags sublimity rainbow-mode beacon clipmon browse-kill-ring company-web xref-js2 nyan-mode php-mode virtualenvwrapper spaceline-all-the-icons use-package spaceline magit airline-themes powerline-evil dumb-jump pug-mode eruby-mode haml-mode grizzl folding web-mode web-beautify color-theme-sanityinc-tomorrow zenburn-theme bubbleberry-theme distinguished-theme moe-theme monokai-theme ac-emacs-eclim company-emacs-eclim eclim yaml-mode tern-auto-complete smex simpleclip rainbow-delimiters org-caldav org-bullets openwith neotree mu4e-maildirs-extension mu4e-alert meghanada markdown-mode lua-mode leuven-theme js2-mode jedi helm-projectile fsharp-mode fixme-mode fill-column-indicator evil-org evil-mu4e evil-leader evil-god-state evil-ediff emojify elpy elfeed-org elfeed-goodies drag-stuff dictionary dictcc calfw auctex ac-math)))
+    (nvm gitconfig-mode gitattributes-mode gitignore-mode beacon diminish spaceline moe-theme jquery-doc web-mode toc-org robe enh-ruby-mode rainbow-mode virtualenvwrapper elpy markdown-mode magit-todos magit flycheck-pos-tip flycheck company-web company-tern company-shell company-quickhelp company-nginx company-inf-ruby company-auctex company-bibtex company-ansible company smartparens rainbow-delimiters mu4e-maildirs-extension evil-mu4e mu4e-alert elfeed-org elfeed-goodies elfeed ido-vertical-mode flx-ido counsel-projectile counsel grizzl projectile evil-god-state evil-surround evil-ediff evil-org evil browse-kill-ring session auto-package-update yaml-mode tern smex php-mode org-ref openwith lua-mode js2-refactor js2-mode haml-mode dictcc comment-tags ag use-package general)))
  '(session-use-package t nil (session)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
