b2612e7842863d385ea87b9341ce37c25e344302
Pascal Huber <pascal.huber@resolved.ch>
2018-08-23 16:07:11 +0200

    Add ess for R scripts

--- init.el
+++ init.el
@@ -7,7 +7,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (nvm gitconfig-mode gitattributes-mode gitignore-mode beacon diminish spaceline moe-theme jquery-doc web-mode toc-org robe enh-ruby-mode rainbow-mode virtualenvwrapper elpy markdown-mode magit-todos magit flycheck company-web company-tern company-shell company-nginx company-inf-ruby company-auctex company-bibtex company-ansible company smartparens rainbow-delimiters mu4e-maildirs-extension evil-mu4e mu4e-alert elfeed-org elfeed-goodies elfeed ido-vertical-mode flx-ido counsel-projectile counsel grizzl projectile evil-god-state evil-surround evil-ediff evil-org evil browse-kill-ring session auto-package-update yaml-mode tern smex php-mode org-ref openwith lua-mode js2-refactor js2-mode haml-mode dictcc comment-tags ag general)))
+    (ess nvm gitconfig-mode gitattributes-mode gitignore-mode beacon diminish spaceline moe-theme jquery-doc web-mode toc-org robe enh-ruby-mode rainbow-mode virtualenvwrapper elpy markdown-mode magit-todos magit flycheck company-web company-tern company-shell company-nginx company-inf-ruby company-auctex company-bibtex company-ansible company smartparens rainbow-delimiters mu4e-maildirs-extension evil-mu4e mu4e-alert elfeed-org elfeed-goodies elfeed ido-vertical-mode flx-ido counsel-projectile counsel grizzl projectile evil-god-state evil-surround evil-ediff evil-org evil browse-kill-ring session auto-package-update yaml-mode tern smex php-mode org-ref openwith lua-mode js2-refactor js2-mode haml-mode dictcc comment-tags ag general)))
  '(session-use-package t nil (session)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
