3731a6c244cb7fc5b0f3965033fe12f13d1169d9
Pascal Huber <pascal.huber@resolved.ch>
2018-10-21 23:35:35 +0200

    improve yasnippet setup for web-mode and add ivy-yasnippet

--- init.el
+++ init.el
@@ -5,12 +5,15 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("dd43ce1171324a8e47f9e4ca9f49773c4b4960706171ab951130c668adc59f53" default)))
  '(org-agenda-files
    (quote
     ("~/git/emacs/configuration.org" "/home/pascal/org/eth.org" "/home/pascal/org/private.org" "/home/pascal/org/quickshift.org")))
  '(package-selected-packages
    (quote
-    (company-go go-mode jedi ess nvm gitconfig-mode gitattributes-mode gitignore-mode beacon diminish spaceline moe-theme jquery-doc web-mode toc-org robe enh-ruby-mode rainbow-mode virtualenvwrapper elpy markdown-mode magit-todos magit flycheck company-web company-tern company-shell company-nginx company-inf-ruby company-auctex company-bibtex company-ansible company smartparens rainbow-delimiters mu4e-maildirs-extension evil-mu4e mu4e-alert elfeed-org elfeed-goodies elfeed ido-vertical-mode flx-ido counsel-projectile counsel grizzl projectile evil-god-state evil-surround evil-ediff evil browse-kill-ring session auto-package-update yaml-mode tern smex php-mode org-ref openwith lua-mode js2-refactor js2-mode haml-mode dictcc comment-tags ag general)))
+    (ivy-yasnippet yasnippet-snippets company-go go-mode jedi ess nvm gitconfig-mode gitattributes-mode gitignore-mode beacon diminish spaceline moe-theme jquery-doc web-mode toc-org robe enh-ruby-mode rainbow-mode virtualenvwrapper elpy markdown-mode magit-todos magit flycheck company-web company-tern company-shell company-nginx company-inf-ruby company-auctex company-bibtex company-ansible company smartparens rainbow-delimiters mu4e-maildirs-extension evil-mu4e mu4e-alert elfeed-org elfeed-goodies elfeed ido-vertical-mode flx-ido counsel-projectile counsel grizzl projectile evil-god-state evil-surround evil-ediff evil browse-kill-ring session auto-package-update yaml-mode tern smex php-mode org-ref openwith lua-mode js2-refactor js2-mode haml-mode dictcc comment-tags ag general)))
  '(session-use-package t nil (session)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
